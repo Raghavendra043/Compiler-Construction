78a84a2f32a299d7616ff03933462edb05dd9b3d
NITHIN0146 <86473764+NITHIN0146@users.noreply.github.com>
2022-02-20 22:47:17 +0530

    Update sl.c

--- sl.c
+++ sl.c
@@ -7,16 +7,19 @@ int main()
     int i,j;
     scanf("%s", Str);
     for (i = 0; Str[i] != '\0'; ++i);
-    for (j = 0; j < i; j++){
-        if(Str[j] == '"'){
-            j++;
+        if(Str[0] == '"'){
             goto there;
 
         }
-    }
-    return 0;
+        else{
+              return 0;
+        }
+
     there:
-    for (j ; j < i; j++){
+    for (j=1 ; j < i; j++){
+            if(Str[j] == ' '){
+                continue;
+            }
         if(Str[j] == '\\'){
                 j++;
             if(Str[j] == '\\'){
@@ -42,7 +45,13 @@ int main()
 
         }
         if(Str[j] == '"'){
-           return 1;
+                if(j = i - 1){
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+
            break;
         }
     }
