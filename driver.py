98fb690c5a307c000a49980e46973b2e51a9d33f
HATTENSCHWEILER <1211afshan@gmail.com>
2022-02-23 21:05:56 +0530

    naming convention my ass

--- driver.py
+++ driver.py
@@ -1,5 +1,5 @@
 import sys
-import Dfa
+import dfa
 from Scanner import bindStream, setLP, errorRead
 from Constants import EOF
 from utils import trim
@@ -18,7 +18,7 @@ if __name__ == '__main__':
                 setLP(lp)
                 # Calls the dfa simulator which returns:
                 # (the token, lineNo(to keep track of line numbers), TokenName)
-                temp = Dfa.DFA()
+                temp = dfa.DFA()
                 nextToken = temp[0]
                 lineNo += temp[1]
                 tokenName = temp[2]
@@ -26,7 +26,7 @@ if __name__ == '__main__':
                 if nextToken == None:   
                     # When none dfas match, it uses panic mode recovery 
                     # to skip the line and move to the next line if there is one.
-                    lp = Dfa.panic_mode()
+                    lp = dfa.panic_mode()
                     o.write('--Error at Line '+str(lineNo)+
                             ':\n\t'+errorRead()+'\n')
                     if lp == -1:
